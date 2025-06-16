4e043552838fbefedaba853f6d75665358932cc5
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2025-06-16 10:56:32 +0300

    #100 - added kyo-like example of SIP-47 method to regression tests

--- shared/src/test/scala/cpstest/TestShiftSIP47.scala
+++ shared/src/test/scala/cpstest/TestShiftSIP47.scala
@@ -15,20 +15,36 @@ object TestSiftSIP47Scope {
     Future.successful(s"${summon[String]}: ${arg1}  ${arg2}")
   }
 
+  // kyo-like definitions
+  trait F
+  trait P[E,A]
+  
+  given F = new F {}
+  given P[String,Int] = new P {}
+
+  def method1[A](using F)(name: String, default: =>A)[E](using P[E,A]): Future[Either[E,A]] = ???
+
 }
 
 class TestShiftSIP47 {
 
-  import TestSiftSIP47Scope.*
+  import TestSiftSIP47Scope.{*,given}
 
   @Test
   def testShiftSIP47(): Unit = {
-    given cps.macros.flags.DebugLevel = cps.macros.flags.DebugLevel(20)
+    //given cps.macros.flags.DebugLevel = cps.macros.flags.DebugLevel(20)
     async[Future] {
       given String = "Hello"
       val result = await(method("arg1", "arg2"))
     }
+  }
 
+  @Test
+  def testShiftSIP47KioLike(): Unit = {
+     val f = async[Future] {
+        val e:Either[String, Int] = method1("toto",  2).await
+        e
+     }
   }
 
 }
